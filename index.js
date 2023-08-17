7025a7af2087e475ad41387804a7859b6e56005f
Woody Ramey <woodyramey@icloud.com>
2023-08-17 00:32:21 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ let main = document.getElementById('main')
 let closeMenu = document.getElementById('closeMenu')
 let header = document.getElementById('head')
 openMenu.addEventListener('click', () => {
-    header.classList.toggle('hidden')
+    // header.classList.toggle('hidden')
 
     main.classList.toggle('active')
 })
 closeMenu.addEventListener('click', () => {
-    header.classList.toggle('hidden')
+    // header.classList.toggle('hidden')
     main.classList.toggle('active')
 })
 
