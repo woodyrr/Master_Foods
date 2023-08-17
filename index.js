ab3fb787367515df8593d73b9ee9704d13d437d5
Woody Ramey <woodyramey@icloud.com>
2023-08-17 00:10:32 -0500

    added animation

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 let openMenu = document.getElementById('openMenu')
 let navOptions = document.querySelector('.nav-options')
+let main = document.getElementById('main')
+let closeMenu = document.getElementById('closeMenu')
+let header = document.getElementById('head')
 openMenu.addEventListener('click', () => {
-    navOptions.classList.toggle('hidden')
+    header.classList.toggle('hidden')
+
+    main.classList.toggle('active')
 })
+closeMenu.addEventListener('click', () => {
+    header.classList.toggle('hidden')
+    main.classList.toggle('active')
+})
+
